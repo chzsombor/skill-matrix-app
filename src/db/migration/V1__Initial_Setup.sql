23455ed91d971f92e698b64b47c9c3c8e30071c3
User <user@kpszk-MacBook-Air.local>
2023-03-17 16:22:15 +0100

    Add db scripts and psql connection

---
+++ src/db/migration/V1__Initial_Setup.sql
@@ -0,0 +1,49 @@
+-- Create role and user
+-- CREATE ROLE alizuser WITH LOGIN PASSWORD 'aliz';
+-- ALTER ROLE alizuser CREATEDB;
+
+-- Create schema (if needed)
+-- CREATE SCHEMA IF NOT EXISTS skillmatrix;
+
+-- Create SkillType table
+CREATE TABLE skill_type (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- Create Skill table
+CREATE TABLE skill (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    skill_type_id INTEGER NOT NULL,
+    FOREIGN KEY (skill_type_id) REFERENCES skill_type(id)
+);
+
+-- Create ProficiencyLevel table
+CREATE TABLE proficiency_level (
+    id SERIAL PRIMARY KEY,
+    level_name VARCHAR(255) NOT NULL UNIQUE,
+    level_value INTEGER NOT NULL UNIQUE
+);
+
+-- Create User table
+CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- Create UserSkill table
+CREATE TABLE user_skill (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    skill_id INTEGER NOT NULL,
+    proficiency_level_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user"(id),
+    FOREIGN KEY (skill_id) REFERENCES skill(id),
+    FOREIGN KEY (proficiency_level_id) REFERENCES proficiency_level(id),
+    UNIQUE(user_id, skill_id)
+);
+
